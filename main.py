a0e336c25d8449159f1681c588995f34fcdda9d8
Giorgio Giuliani <giorgiogiuliani@C02FC10NML85.homenet.telecomitalia.it>
2022-09-29 10:56:04 +0200

    added time planned execution

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import yagmail
 import time
+from datetime import datetime as dt
 
 sender = 'ggglnix@gmail.com'
-receiver = 'nakigi4629@canyona.com'
+receiver = 'bianco.cinzia@yahoo.it'
 subject = 'automated email'
 contents = """
 Here is the content of the email!
@@ -10,12 +11,16 @@ Ciao!
 """
 i = 0
 
-while True:
-  time.sleep(30)
-  yag = yagmail.SMTP(user=sender, password='ybhztkldfrckmklu')
-  subject = subject + str(i)
-  yag.send(to=receiver, subject=subject, contents=contents)
-  ++i
+while True: #an email every 30 seconds
+    now = dt.now()
+    print(now)
+    if (now.hour == 10 and now.minute == 53):
+        yag = yagmail.SMTP(user=sender, password='ybhztkldfrckmklu')
+        subject = subject + str(i)
+        yag.send(to=receiver, subject=subject, contents=contents)
+        print('email sent '+str(i))
+        ++i
+        time.sleep(60)
   
 
 
